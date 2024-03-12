1b16383c9e5ce909ea50a5f886bd815afc88b833
OlehHrytsiv <163109374+OlehHrytsiv@users.noreply.github.com>
2024-03-12 09:36:09 +0100

    Zadanie1

---
+++ Zadanie_1/app.js
@@ -0,0 +1,40 @@
+
+// Zadanie 3
+const student = require('./student')
+
+const FULLNAME = student.getStudentFullName()
+const STUDENT_ID = student.getStudentId()
+
+console.log(`MySUDENT_ID name is ${FULLNAME}. My student ID is ${STUDENT_ID}`)
+
+// Zadanie 4
+const http = require('http')
+const PORT = 3000
+
+let server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end('Jestem tutaj)!\n')
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+    console.log(`My name is ${FULLNAME}. My student ID is ${STUDENT_ID}.`)
+});
+
+// Zadanie 5
+server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    const htmlContent = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>About student</title>
+        </head>
+        <body>
+            <p>My name is ${FULLNAME}. My student ID is ${STUDENT_ID}.</p>
+        </body>
+        </html>
+    `;
+    res.end(htmlContent)
+});
+
